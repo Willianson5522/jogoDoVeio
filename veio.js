7e229a8b8ee979fb54ad16dcb0e61f2162c17f93
Alisson Silva <94844351+Alisson589@users.noreply.github.com>
2022-06-23 22:53:34 -0300

    Fase final

--- veio.js
+++ veio.js
@@ -1,7 +1,60 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const winninMessageTextElement = document.querySelector('[data-winning-message-text]')
+const restarButton = document.querySelector('[data-restart-button]')
 
-let isCircleTurn = false;
+const msgVitoria = document.querySelector("[data-winning-msg]")
+
+    let isCircleTurn = false;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+const startgame = () => {
+    for (const cell of cellElements) {
+        cell.classList.remove("circle");
+        cell.classList.remove("x");
+        cell.removeEventListener("click", handleclick);
+        cell.addEventListener("click", handleclick, { once: true });
+    }
+
+    isCircleTurn = false;
+
+    board.classList.add("x");
+    msgVitoria.classList.remove("show-msgVitoria")
+
+}
+
+const endGame = (isDraw) => {
+    if (isDraw) {
+        winninMessageTextElement.innerText = "Empate!"
+    } else {
+        winninMessageTextElement.innerText = isCircleTurn 
+        ? "0 Venceu" 
+        : "X Venceu";
+    }
+
+    msgVitoria.classList.add("show-msgVitoria")
+
+};
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some((combination) => {
+        return combination.every((index) => {
+            return cellElements[index].classList.contains(currentPlayer);
+        });
+    });
+};
+
+
 
 const placeMark = (cell, classToAdd) => {
     cell.classList.add(classToAdd);
@@ -29,13 +82,17 @@ const handleclick = (e) => {
 
     placeMark(cell, classToAdd);
     // Verificar por Vitoria
+    const isWin = checkForWin(classToAdd);
+
+    if (isWin) {
+        endGame(false)
+    }
     // Verificar por empate
     // Mudar simbolo
 
     swapTurns();
 };
 
+startgame();
 
-for (const cell of cellElements) {
-    cell.addEventListener("click", handleclick, { once: true });
-}
+restarButton.addEventListener("click", startgame);
